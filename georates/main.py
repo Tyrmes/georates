048e81a5ef5f233c5db18a87752214b38e0f7612
cgonzalez7 <72471537+cgonzalez7@users.noreply.github.com>
2022-06-03 10:11:23 -0500

    Requested changes already done. I converted the negative values to positive one but not sure if it will be better to convert it to zero instead. GEOR-10_New_Data

--- georates/main.py
+++ georates/main.py
@@ -4,19 +4,23 @@ import gstools as gs
 import matplotlib.pyplot as plt
 from math import radians
 # %% Define values
+# Generate random values
 x = np.random.RandomState(123).rand(100) * 100
 y = np.random.RandomState(223).rand(100) * 100
-model = gs.Exponential(dim=2, var=2, len_scale=10)
+model = gs.Exponential(dim=2, var=2, len_scale=[10, 8], angles=[radians(130), radians(70)])
 srf = gs.SRF(model, mean=0, seed=123)
+# Thickness
 field = srf((x, y))
-print(field)
+field = np.where(field < 0, (field * -1), field)
+# Coordinates
+df = pd.DataFrame({'X': x, 'Y': y})
 
 #%% Create semivariogram
 
 # Name of columns
 X = "X"
 Y = "Y"
-H = "h"
+
 # Parameters of covariance model
 dim = 2
 var = 2
@@ -28,7 +32,7 @@ angles = radians(180)
 # Position (Lat, long)
 pos = df[[X, Y]].values.T
 # Values -> Thickness
-field = df[H].values.T
+field = field.T
 
 # Estimate the variogram
 bin_center, dir_vario = gs.vario_estimate(
