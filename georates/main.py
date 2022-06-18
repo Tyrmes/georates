c8fb12d7ab17cc5edc96704449a64519b72c4563
cgonzalez7 <72471537+cgonzalez7@users.noreply.github.com>
2022-06-17 19:32:08 -0500

    First attempt for adjusting variogram, but the results are not good. GEOR-10_New_Data

--- georates/main.py
+++ georates/main.py
@@ -27,27 +27,33 @@ var = 2
 len_scale = 8
 angles_tol = np.pi / 16
 bandwidth = 8
-angles = radians(180)
+angles = [radians(130), radians(70)]
 
 # Position (Lat, long)
 pos = df[[X, Y]].values.T
+# x = df[X].values.T
+# y = df[Y].values.T
 # Values -> Thickness
 field = field.T
 
 # Estimate the variogram
+bins = range(0, 40, 2)
 bin_center, dir_vario = gs.vario_estimate(
-    pos, field, angles=angles, angles_tol=angles_tol, bandwidth=bandwidth
-)
+    pos, field, bins, direction=gs.rotated_main_axes(dim=dim, angles=angles),
+    angles_tol=angles_tol, bandwidth=bandwidth)
 
 # Fit the variogram with a covariance model
-model = gs.Gaussian(dim=dim, angles=angles)
+#model = gs.Gaussian(dim=dim, angles=angles)
 model.fit_variogram(bin_center, dir_vario)
 
 # Plot the fitting model
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-ax.scatter(bin_center, dir_vario, label="Empirical semivariogram")
-model.plot(ax=ax)
-ax.legend()
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+ax1.scatter(bin_center, dir_vario[0], label="Empirical semivariogram")
+ax1.scatter(bin_center, dir_vario[1], label="Empirical semivariogram 2")
+model.plot("vario_axis", axis=0, ax=ax1, label='fit on axis=0')
+model.plot("vario_axis", axis=1, ax=ax1, label="fit on axis 1")
+ax1.legend()
+srf.plot(ax=ax2)
 plt.show()
-print(model)
+#print(model)
 
